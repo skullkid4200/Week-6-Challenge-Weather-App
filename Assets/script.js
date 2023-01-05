0f2175be3be7a05261042b5fffd1cccae5099cab
skullkid4200 <jackeinhorn123@gmail.com>
2023-01-05 17:05:03 -0500

    got some BASIC html and stuff setup, linked jQuery, linked bootstrap, introduced the weather API

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+console.log($);
+console.log(typeof $);
+
+function weatherBalloon(cityID) {
+    var key = 'da6641fab750455262ef12ca3de15fb6';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
+        .then(function (resp) { return resp.json() }) // Convert data to json
+        .then(function (data) {
+            drawWeather(data);
+        })
+        .catch(function () {
+            // catch any errors
+        });
+}
+
+window.onload = function () {
+    weatherBalloon(6167865);
+}
+
+function drawWeather(d) {
+    var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+    var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
+
+    document.getElementById('description').innerHTML = d.weather[0].description;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+}
